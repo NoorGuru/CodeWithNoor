bfab22ae740d48a8567fae6cf3f2bef92a354513
mohnoor94 <moh.noor94@gmail.com>
2020-12-06 21:00:49 +0200

    Update site icon, and other minor changes

--- english/src/components/layout.js
+++ english/src/components/layout.js
@@ -9,10 +9,13 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+import 'bootstrap/dist/css/bootstrap.css';
+
 import Header from "./header"
 import Footer from "./footer"
 
 import style from "../styles/layout.module.scss"
+
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
